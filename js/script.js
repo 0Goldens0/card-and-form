9412b74468c3e5de6d438f9d3872910e7dd2ed1e
Ал <EcoKozhevnikov@yandex.ru>
2022-06-28 17:11:11 +0300

    card_and_form

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $("form").submit(function () {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        // Добавление решётки к имени ID
+        var formNm = $('#' + formID);
+        $.ajax({
+            type: "POST",
+            url: 'mail.php',
+            data: formNm.serialize(),
+            success: function (data) {
+                // Вывод текста результата отправки
+                $(formNm).html(data); 
+            },
+            error: function (jqXHR, text, error) {
+                // Вывод текста ошибки отправки
+                $(formNm).html(error);         
+            }
+        });
+        return false;
+    });
+});
+
+document.querySelector('#btn-lg').addEventListener('click', () => {
+    let h = document.querySelector('#lg').textContent;
+    document.getElementById('pn').value = h;
+})
+
+document.querySelector('#btn-phil').addEventListener('click', () => {
+    let h = document.querySelector('#phlips').textContent;
+    document.getElementById('pn').value = h;
+})
+
+document.querySelector('#btn-sony').addEventListener('click', () => {
+    let h = document.querySelector('#sony').textContent;
+    document.getElementById('pn').value = h;
+})
+
+document.querySelector('#btn-xia').addEventListener('click', () => {
+    let h = document.querySelector('#xiaomi').textContent;
+    document.getElementById('pn').value = h;
+})
